188c831cd0e010912f0f44ea47ab3c0804bc2b76
user <muigastephen14@gmail.com>
2026-02-10 04:56:32 +0300

    day 1

--- arrowf.js
+++ arrowf.js
@@ -14,3 +14,17 @@ console.log(greet('Dwayne'))
 
 const valentines = name => `Be my valentines, ${name}`
 console.log(valentines('Sharon'))
+
+
+// No parameters
+const sayHi = () => 'Hi!';
+
+// One parameter (parentheses optional)
+const double = x => x * 2;
+const doubleExplicit = (x) => x * 2; // Same thing
+
+// Multiple parameters (parentheses required)
+const add = (a, b) => a + b;
+
+// Returning an object (wrap in parentheses)
+const makePerson = (name, age) => ({ name, age });
